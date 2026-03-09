daa1ad06d9995bc2244c45e1cc22400792efc84c
ThiagoCosta <thiagoacnascimento@gmail.com>
2026-03-09 09:27:52 -0400

    feat: add `FormNotificacao` section

--- src/schema/formSchema.ts
+++ src/schema/formSchema.ts
@@ -4,6 +4,7 @@ import { qualificacaoSchema } from "./qualificacaoSchema";
 import { detalheSchema } from "./detalheSchema";
 import { motivacaoSchema } from "./motivacaoSchema";
 import { dados } from "./dadosSchema";
+import { notificacaoSchema } from "./notificacaoSchema";
 
 export const schema = z.object({
   ...identidadeSchema.shape,
@@ -11,6 +12,7 @@ export const schema = z.object({
   ...detalheSchema.shape,
   ...motivacaoSchema.shape,
   ...dados.shape,
+  ...notificacaoSchema.shape,
 });
 
 export type FormData = z.infer<typeof schema>;
---
+++ src/schema/notificacaoSchema.ts
@@ -0,0 +1,7 @@
+import z from "zod";
+
+export const notificacaoSchema = z.object({
+  tipo_de_notificacao: z
+    .array(z.string())
+    .min(1, "Selecione pelo menos um meio de notificação."),
+});
