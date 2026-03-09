46edfffb1fa5f539ac7fd14b1cf1fb86035b6119
ThiagoCosta <thiagoacnascimento@gmail.com>
2026-03-09 09:57:36 -0400

    chore: remove sub title

--- src/components/Formulario/formHeader.tsx
+++ src/components/Formulario/formHeader.tsx
@@ -4,9 +4,6 @@ export default function FormHeader() {
       <h2 className="text-center text-pv-green-600 font-bold text-xl md:text-2xl mb-8">
         Requerimento de Restituição de Tributos e Rendas Municipais
       </h2>
-      <p className="text-pv-gray-700 mb-6 text-center">
-        Preencha todos os campos obrigatórios para solicitar a restituição
-      </p>
     </div>
   );
 }
