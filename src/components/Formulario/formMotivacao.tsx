699ef064b047cf618907cf0ed1684e9d2f5a2d31
ThiagoCosta <thiagoacnascimento@gmail.com>
2026-03-10 12:07:48 -0400

    feat: add `Property Data` sesstion to `FormMotivacao`

--- src/components/Formulario/formMotivacao.tsx
+++ src/components/Formulario/formMotivacao.tsx
@@ -1,11 +1,18 @@
 "use client";
+
 import { useFormContext } from "react-hook-form";
 
 export default function FormMotivacao() {
   const {
     register,
+    watch,
     formState: { errors },
   } = useFormContext();
+
+  const motivo = watch("motivo_principal");
+
+  const mostrarImovel =
+    motivo === "Pagamento referente a imóvel incorreto (IPTU/TRSD)";
 
   return (
     <section className="form-section p-5 bg-[#FAFAFA] border border-gray-300 rounded mt-10 mb-10">
@@ -68,6 +75,61 @@ export default function FormMotivacao() {
           <span>Decisão administrativa/judicial</span>
         </label>
       </div>
+
+      {/* CAMPOS CONDICIONAIS */}
+
+      {mostrarImovel && (
+        <div className="bg-gray-50 p-4 rounded border border-gray-200 mb-8">
+          <h4 className="font-semibold mb-4 text-[#2c3e50]">
+            Dados dos Imóveis
+          </h4>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div>
+              <label className="block font-semibold mb-1">
+                Inscrição Imóvel Correto <span className="text-red-500">*</span>
+              </label>
+
+              <input
+                {...register("imovel_correto")}
+                placeholder="Ex: 123456789"
+                className={`w-full border-2 rounded p-2 ${
+                  errors.imovel_correto ? "border-red-500" : "border-gray-400"
+                }`}
+              />
+
+              {errors.imovel_correto && (
+                <p className="text-red-500 text-sm">
+                  {errors.imovel_correto.message as string}
+                </p>
+              )}
+            </div>
+
+            <div>
+              <label className="block font-semibold mb-1">
+                Inscrição Imóvel Incorreto{" "}
+                <span className="text-red-500">*</span>
+              </label>
+
+              <input
+                {...register("imovel_incorreto")}
+                placeholder="Ex: 987654321"
+                className={`w-full border-2 rounded p-2 ${
+                  errors.imovel_incorreto ? "border-red-500" : "border-gray-400"
+                }`}
+              />
+
+              {errors.imovel_incorreto && (
+                <p className="text-red-500 text-sm">
+                  {errors.imovel_incorreto.message as string}
+                </p>
+              )}
+            </div>
+          </div>
+        </div>
+      )}
+
+      {/* DESCRIÇÃO */}
 
       <div className="w-full mt-4">
         <label className="block font-semibold mb-2 text-gray-700">
--- src/schema/motivacaoSchema.ts
+++ src/schema/motivacaoSchema.ts
@@ -1,22 +1,35 @@
 import z from "zod";
 
-export const motivacaoSchema = z.object({
-  motivo_principal: z
-    .string()
-    .refine(
-      (value) =>
-        [
-          "Pagamento em duplicidade",
-          "Erro na identificação do sujeito passivo",
-          "Erro na alíquota, base de cálculo ou apuração do valor",
-          "Pagamento referente a imóvel incorreto (IPTU/TRSD)",
-          "Decisão administrativa/judicial",
-        ].includes(value),
-      {
-        message: "Selecione um motivo principal!",
-      },
-    ),
-  descicao_da_motivacao: z
-    .string()
-    .min(10, "Descreva pelo menos 10 caracteres"),
-});
+export const motivacaoSchema = z
+  .object({
+    motivo_principal: z.string(),
+
+    descicao_da_motivacao: z
+      .string()
+      .min(10, "Descreva pelo menos 10 caracteres"),
+
+    imovel_correto: z.string().optional(),
+    imovel_incorreto: z.string().optional(),
+  })
+  .superRefine((data, ctx) => {
+    if (
+      data.motivo_principal ===
+      "Pagamento referente a imóvel incorreto (IPTU/TRSD)"
+    ) {
+      if (!data.imovel_correto || data.imovel_correto.length < 5) {
+        ctx.addIssue({
+          path: ["imovel_correto"],
+          code: "custom",
+          message: "Informe a inscrição correta",
+        });
+      }
+
+      if (!data.imovel_incorreto || data.imovel_incorreto.length < 5) {
+        ctx.addIssue({
+          path: ["imovel_incorreto"],
+          code: "custom",
+          message: "Informe a inscrição incorreta",
+        });
+      }
+    }
+  });
